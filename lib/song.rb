1d9ed109bb715e8fa3ad1362e7b7d478bc8db640
Kristian Joos <kjoos22@gmail.com>
2020-09-28 21:39:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,11 @@ class Song
   #instance variables
   attr_accessor :name, :artist, :genre
   
-  
   #initialization accepts name of song, artist, and genre
   def initialize(name, artist, genre)
-    
+    @name = name
+    @artist = artist
+    @genre = genre
   end
   
 end
