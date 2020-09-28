3f0daf5b592c8296ef804a3da336f22e03160bab
Kristian Joos <kjoos22@gmail.com>
2020-09-28 21:42:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,21 @@
 class Song
-  #class variables
+  #class variable(s)
   @@count = 0   #tracks the number of instances of Song
   
   #instance variables
   attr_accessor :name, :artist, :genre
   
-  #initialization accepts name of song, artist, and genre
+  #initialization - accepts name of song, artist, and genre
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+  end
+  
+  #class method(s)
+  def self.count    #returns @@count, the total number of instances of Song
+    @@count
   end
   
 end
